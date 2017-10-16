dd6c8eb7f493908f60b06991fbc7e1b1f57de4e4
ravikums <raviuit14@gmail.com>
2017-10-16 15:44:07 +0530

    added sop

--- src/com/essilor/Logslf4J.java
+++ src/com/essilor/Logslf4J.java
@@ -9,10 +9,14 @@ import org.slf4j.LoggerFactory;
 public class Logslf4J {
     // private static Logger LOGGER= Logger.getLogger(LoggingTestLog4J.class.getName());
     private static Logger LOGGER = LoggerFactory.getLogger(Logslf4J.class);
+
     public static void main(String[] args) {
         //  LOGGER.setLevel(Level.INFO);
         LOGGER.info("Hello in Logger");
         // LOGGER.severe("Hello level is severe");
+
+        System.out.println("hai");
+
 
     }
 }
